d8122fe7aad64949b21ab57fb71daf1824712506
tugwellchristi <tugwellchristi@gmail.com>
2023-12-30 19:17:58 -0800

    updated schema,added content to category.js and index.js in the models folder

--- db/schema.sql
+++ db/schema.sql
@@ -3,29 +3,29 @@ CREATE DATABASE ecommerce_db;
 
 Use ecommerce_db;
 
-CREATE TABLE category (
+CREATE TABLE Category (
     id INT AUTO_INCREMENT PRIMARY KEY,
     category_name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE product (
+CREATE TABLE Product (
     id INT AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(50) NOT NULL,
     price DECIMAL(10, 2) NOT NULL CHECK (decimal >= 0),
     stock INT NOT NULL DEFAULT 10 CHECK (integer >= 0),
     category_id INT,
-    FOREIGN KEY (category_id) REFERENCES category(id)
+    FOREIGN KEY (category_id) REFERENCES Category(id)
 );
 
-CREATE TABLE tag (
+CREATE TABLE Tag (
     id INT AUTO_INCREMENT PRIMARY KEY,
     tag_name VARCHAR(50)
 );
 
-CREATE TABLE productTag (
+CREATE TABLE ProductTag (
     id INT AUTO_INCREMENT PRIMARY KEY,
     product_id INT,
     tag_id INT,
-    FOREIGN KEY (product_id) REFERENCES product(id),
-    FOREIGN KEY (tag_id) REFERENCES tag(id)  
+    FOREIGN KEY (product_id) REFERENCES Product(id),
+    FOREIGN KEY (tag_id) REFERENCES Tag(id)  
 );
